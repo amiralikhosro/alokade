6748231781672e263e24b5205b81032d9d71838e
Ali Khosro <ali.khosro72@gmail.com>
2019-03-08 23:00:07 +0330

    add address page && create address page

---
+++ pages/profile/createAddress.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ali_khosro
+ * Date: 3/8/2019
+ * Time: 3:50 PM
+ */
---
+++ pages/profile/personalInformation.php
@@ -0,0 +1,19 @@
+<div class="col-12">
+    <div class="row">
+        <div class="input-group mb-3 col-6">
+            <input type="text" class="form-control" placeholder="نام" aria-label="nameInput" aria-describedby="name">
+            <div class="input-group-append">
+                <span class="position-absolute avatar d-block p-3" id="nameInput"></span>
+            </div>
+        </div>
+        <div class="input-group mb-3 col-6">
+            <input type="text" class="form-control" placeholder="نام خانوادگی" aria-label="lastNameInput" aria-describedby="name">
+            <div class="input-group-append">
+                <span class="position-absolute avatar d-block p-3" id="lastNameInput"></span>
+            </div>
+        </div>
+        <div class="input-group mb-3 col-6">
+            <input type="text" class="example1" placeholder=""/>
+        </div>
+    </div>
+</div>
